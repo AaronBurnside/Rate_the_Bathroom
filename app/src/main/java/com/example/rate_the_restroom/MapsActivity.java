176bc9daf6d46eca0163de24f6b255d0341b7f0f
AaronBurnside <aburns29@byui.edu>
2021-10-05 18:35:38 -0600

    start

--- app/src/main/java/com/example/rate_the_restroom/MapsActivity.java
+++ app/src/main/java/com/example/rate_the_restroom/MapsActivity.java
@@ -9,19 +9,21 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.rate_the_restroom.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
 
     private GoogleMap mMap;
-
+    private Marker STCMarker;
 
     private ActivityMapsBinding binding;
     static final LatLng CP1 = new LatLng(43.811452, -111.786787);
     static final LatLng CP2 = new LatLng(43.821926, -111.786798);
     static final LatLng CP3 = new LatLng(43.821791, -111.778140);
     static final LatLng CP4 = new LatLng(43.811542, -111.778128);
+    static final LatLng CentP = new LatLng(43.817700, -111.783912);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,25 +38,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setOnMarkerClickListener(this);
         // Add a marker in Sydney and move the camera
-        //LatLng sydney = new LatLng(-34, 151);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CP1, 15));
-        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        googleMap.getUiSettings().setZoomGesturesEnabled(false);
+        LatLng STCBuilding = new LatLng(43.814729, -111.784617);
+        STCMarker = mMap.addMarker(new MarkerOptions().position(STCBuilding).title("Science and Technology Center"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CentP, 25));
 
     }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        if (marker.equals(STCMarker))
+        {
+            // activate next activity
+        }
+        return false;
+    }
+
 }
